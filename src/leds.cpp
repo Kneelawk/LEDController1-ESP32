cb250a47641a2e3463231adc779723af4bb3da16
Kneelawk <kneelawk@gmail.com>
2021-09-14 17:24:47 -0700

    Add hue-per-pixel and hue-per-frame endpoints

--- src/leds.cpp
+++ src/leds.cpp
@@ -12,12 +12,19 @@ namespace leds {
 const int DATA_PIN = 4;
 
 const uint8_t DEFAULT_BRIGHTNESS = 96;
-const uint8_t DEFAULT_SATURATION = 240;
 const uint32_t DEFAULT_FRAME_DURATION = 20;
+const uint8_t DEFAULT_HUE_PER_FRAME = 1;
+const uint8_t DEFAULT_HUE_PER_PIXEL = 3;
+const uint8_t DEFAULT_SATURATION = 240;
 
-uint8_t brightness = DEFAULT_BRIGHTNESS;
 uint8_t hue = 0;
 CRGB leds[NUM_LEDS];
+
+uint8_t brightness = DEFAULT_BRIGHTNESS;
+
+uint8_t huePerFrame = DEFAULT_HUE_PER_FRAME;
+
+uint8_t huePerPixel = DEFAULT_HUE_PER_PIXEL;
 
 uint32_t frameDuration = DEFAULT_FRAME_DURATION;
 uint32_t lastFrame = 0;
@@ -53,10 +60,12 @@ void setupLeds() {
 
 void setupServer(AsyncWebServer *server) {
     // Setup brightness GET endpoint
-    server->on("/brightness", HTTP_GET, [](AsyncWebServerRequest *request) {
+    ArRequestHandlerFunction brightnessGet = [](
+            AsyncWebServerRequest *request) {
         String brightnessStr = String(brightness);
         request->send_P(200, "text/plain", brightnessStr.c_str());
-    });
+    };
+    server->on("/brightness", HTTP_GET, brightnessGet);
 
     // Setup brightness PUT endpoint
     AsyncCallbackWebHandler *brightnessPut = new AsyncCallbackWebHandler();
@@ -68,21 +77,20 @@ void setupServer(AsyncWebServer *server) {
         char *dataStr = nullTerminate(data, len);
 
         // Convert data string to a number and assign it to brightness
-        brightness = strtol(dataStr, nullptr, 10);
+        brightness = (uint8_t) strtol(dataStr, nullptr, 10);
 
         delete[] dataStr;
     });
-    brightnessPut->onRequest([](AsyncWebServerRequest *request) {
-        String brightnessStr = String(brightness);
-        request->send_P(200, "text/plain", brightnessStr.c_str());
-    });
+    brightnessPut->onRequest(brightnessGet);
     server->addHandler(brightnessPut);
 
     // Setup frame-duration GET endpoint
-    server->on("/frame-duration", HTTP_GET, [](AsyncWebServerRequest *request) {
+    ArRequestHandlerFunction frameDurationGet = [](
+            AsyncWebServerRequest *request) {
         String frameDurationStr = String(frameDuration);
         request->send_P(200, "text/plain", frameDurationStr.c_str());
-    });
+    };
+    server->on("/frame-duration", HTTP_GET, frameDurationGet);
 
     // Setup frame-duration PUT endpoint
     AsyncCallbackWebHandler *frameDurationPut = new AsyncCallbackWebHandler();
@@ -93,15 +101,54 @@ void setupServer(AsyncWebServer *server) {
         // Construct a null-terminated string out of the data
         char *dataStr = nullTerminate(data, len);
 
-        frameDuration = strtol(dataStr, nullptr, 10);
+        frameDuration = (uint32_t) strtol(dataStr, nullptr, 10);
 
         delete[] dataStr;
     });
-    frameDurationPut->onRequest([](AsyncWebServerRequest *request) {
-        String frameDurationStr = String(frameDuration);
-        request->send_P(200, "text/plain", frameDurationStr.c_str());
-    });
+    frameDurationPut->onRequest(frameDurationGet);
     server->addHandler(frameDurationPut);
+
+    // Setup hue-per-pixel GET endpoint
+    ArRequestHandlerFunction huePerPixelGet = [](
+            AsyncWebServerRequest *request) {
+        String huePerPixelStr = String(huePerPixel);
+        request->send_P(200, "text/plain", huePerPixelStr.c_str());
+    };
+    server->on("/hue-per-pixel", HTTP_GET, huePerPixelGet);
+
+    // Setup hue-per-pixel PUT endpoint
+    AsyncCallbackWebHandler *huePerPixelPut = new AsyncCallbackWebHandler();
+    huePerPixelPut->setUri("/hue-per-pixel");
+    huePerPixelPut->setMethod(HTTP_PUT);
+    huePerPixelPut->onBody([](AsyncWebServerRequest *request, uint8_t *data,
+                              size_t len, size_t index, size_t total) {
+        char *dataStr = nullTerminate(data, len);
+        huePerPixel = (uint8_t) strtol(dataStr, nullptr, 10);
+        delete[] dataStr;
+    });
+    huePerPixelPut->onRequest(huePerPixelGet);
+    server->addHandler(huePerPixelPut);
+
+    // Setup hue-per-frame GET endpoint
+    ArRequestHandlerFunction huePerFrameGet = [](
+            AsyncWebServerRequest *request) {
+        String huePerFrameStr = String(huePerFrame);
+        request->send_P(200, "text/plain", huePerFrameStr.c_str());
+    };
+    server->on("/hue-per-frame", HTTP_GET, huePerFrameGet);
+
+    // Setup hue-per-frame PUT endpoint
+    AsyncCallbackWebHandler *huePerFramePut = new AsyncCallbackWebHandler();
+    huePerFramePut->setUri("/hue-per-frame");
+    huePerFramePut->setMethod(HTTP_PUT);
+    huePerFramePut->onBody([](AsyncWebServerRequest *request, uint8_t *data,
+                              size_t len, size_t index, size_t total) {
+        char *dataStr = nullTerminate(data, len);
+        huePerFrame = (uint8_t) strtol(dataStr, nullptr, 10);
+        delete[] dataStr;
+    });
+    huePerFramePut->onRequest(huePerFrameGet);
+    server->addHandler(huePerFramePut);
 }
 
 void update() {
@@ -109,11 +156,11 @@ void update() {
     if (now < lastFrame || now >= lastFrame + frameDuration) {
         lastFrame = now;
 
-        custom_fill_rainbow(leds, NUM_LEDS, hue, 3, brightness,
+        custom_fill_rainbow(leds, NUM_LEDS, hue, huePerPixel, brightness,
                             DEFAULT_SATURATION);
         FastLED.show();
 
-        hue++;
+        hue += huePerFrame;
     }
 }
 
